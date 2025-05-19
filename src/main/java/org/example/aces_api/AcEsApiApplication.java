fe432ddfa6e31dc93d59de8c999705426362280e
Rodrigo Simão <rodrigoip435@gmail.com>
2025-05-19 08:12:31 -0300

    first commit

---
+++ src/main/java/org/example/aces_api/AcEsApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.aces_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcEsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcEsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/aces_api/AcEsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.aces_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcEsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
