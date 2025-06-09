bdfa5f58f23665af5f5f1309c79be861c6a23ae3
Rodrigo Simão <rodrigoip435@gmail.com>
2025-06-09 15:24:01 -0300

    feat: adiciona entity Area

---
+++ src/main/java/org/example/aces_api/model/entity/Area.java
@@ -0,0 +1,112 @@
+package org.example.aces_api.model.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Area")
+public class Area {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "nome", length = 100)
+    private String nome;
+
+    @Column(name = "descricao", columnDefinition = "TEXT")
+    private String descricao;
+
+    @Column(name = "regiao", length = 50)
+    private String regiao;
+
+    @Column(name = "populacaoAprox")
+    private int populacaoAprox;
+
+    @Column(name = "nivelRisco", length = 20)
+    private String nivelRisco;
+
+    @Column(name = "prioridade", length = 20)
+    private String prioridade;
+
+    @Column(name = "dataUltimaAtt")
+    private LocalDateTime dataUltimaAtt;
+
+    public Area() {
+    }
+
+    public Area(String nome, String descricao, String regiao, int populacaoAprox, String nivelRisco, String prioridade, LocalDateTime dataUltimaAtt) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.regiao = regiao;
+        this.populacaoAprox = populacaoAprox;
+        this.nivelRisco = nivelRisco;
+        this.prioridade = prioridade;
+        this.dataUltimaAtt = dataUltimaAtt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getRegiao() {
+        return regiao;
+    }
+
+    public void setRegiao(String regiao) {
+        this.regiao = regiao;
+    }
+
+    public int getPopulacaoAprox() {
+        return populacaoAprox;
+    }
+
+    public void setPopulacaoAprox(int populacaoAprox) {
+        this.populacaoAprox = populacaoAprox;
+    }
+
+    public String getNivelRisco() {
+        return nivelRisco;
+    }
+
+    public void setNivelRisco(String nivelRisco) {
+        this.nivelRisco = nivelRisco;
+    }
+
+    public String getPrioridade() {
+        return prioridade;
+    }
+
+    public void setPrioridade(String prioridade) {
+        this.prioridade = prioridade;
+    }
+
+    public LocalDateTime getDataUltimaAtt() {
+        return dataUltimaAtt;
+    }
+
+    public void setDataUltimaAtt(LocalDateTime dataUltimaAtt) {
+        this.dataUltimaAtt = dataUltimaAtt;
+    }
+}
