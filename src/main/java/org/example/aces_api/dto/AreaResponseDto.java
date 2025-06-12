f2303b23596fd7dcdeaba4f669a8b688e228f146
Rodrigo Simão <rodrigoip435@gmail.com>
2025-06-12 18:00:01 -0300

    feat(HATEOAS): Adiciona compatibilidade HATEOAS ao AreaResponseDto

--- src/main/java/org/example/aces_api/dto/AreaResponseDto.java
+++ src/main/java/org/example/aces_api/dto/AreaResponseDto.java
@@ -1,9 +1,14 @@
 package org.example.aces_api.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
 import java.time.LocalDateTime;
 
+@JsonPropertyOrder({"id", "nome", "descricao", "regiao", "populacaoAprox", "nivelRisco", "prioridade", "dataUltimaAtt"})
 public record AreaResponseDto(
-        int id,
+        @JsonProperty("id") // Se quiser que o campo 'id' seja chamado 'id' no JSON, mesmo que o campo interno seja outro (Ex: key)
+        Integer id, // Use Integer para compatibilidade com HATEOAS
         String nome,
         String descricao,
         String regiao,
