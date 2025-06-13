64ffe73b508a7fe004a0324d2c56bca21252ff6b
Luan Henique Campos Soares <101595196+lhcamposs@users.noreply.github.com>
2025-06-13 14:41:24 -0300

    feat: metodo para contar focos
    Metodo no repository para contar os focos do mosquito

--- src/main/java/org/example/aces_api/model/repository/FocoAedesRepository.java
+++ src/main/java/org/example/aces_api/model/repository/FocoAedesRepository.java
@@ -2,8 +2,15 @@ package org.example.aces_api.model.repository;
 
 import org.example.aces_api.model.entity.Area;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
 
 @Repository
 public interface FocoAedesRepository extends JpaRepository<Area, Long> {
+
+    @Query("SELECT COUNT(f) FROM FocoAedes f WHERE f.visita.endereco.area.id = :areaId " +
+            "AND f.visita.dataHora BETWEEN :inicio AND :fim")
+    int countFocosByAreaAndPeriod(Long areaId, LocalDateTime inicio, LocalDateTime fim);
 }
