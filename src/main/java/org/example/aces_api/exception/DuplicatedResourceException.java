e891fb6d029f0e4d99c7ede39d43e37e5d644fe4
Guilherme Duarte <guiduart3223@gmail.com>
2025-06-15 15:12:44 -0300

    Tratamento de Excecao para duplicidade

--- src/main/java/org/example/aces_api/exception/DuplicatedResourceException.java
+++ src/main/java/org/example/aces_api/exception/DuplicatedResourceException.java
@@ -1,7 +1,12 @@
 package org.example.aces_api.exception;
 
 public class DuplicatedResourceException extends RuntimeException {
-  public DuplicatedResourceException(String message) {
-    super(message);
-  }
+
+    public DuplicatedResourceException(String message) {
+        super(message);
+    }
+
+    public DuplicatedResourceException(String resourceName, String fieldName, Object fieldValue) {
+        super(String.format("%s já existe com %s: '%s'", resourceName, fieldName, fieldValue));
+    }
 }
