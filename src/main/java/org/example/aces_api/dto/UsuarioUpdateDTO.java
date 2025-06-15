6d5b2d7f6f1cd40d0a452634c344a190542e2d16
Guilherme Duarte <guiduart3223@gmail.com>
2025-06-15 15:14:10 -0300

    DTO para atualizacao de usuario

--- src/main/java/org/example/aces_api/dto/UsuarioUpdateDTO.java
+++ src/main/java/org/example/aces_api/dto/UsuarioUpdateDTO.java
@@ -1,4 +1,9 @@
 package org.example.aces_api.dto;
 
-public record UsuarioUpdateDTO() {
+public record UsuarioUpdateDTO(
+        String nome,
+        String telefone,
+        String cargo,
+        String email
+) {
 }
