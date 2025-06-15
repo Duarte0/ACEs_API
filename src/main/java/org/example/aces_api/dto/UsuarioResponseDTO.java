1f3d936808a9179da024d7ff57964dd6eb016892
Guilherme Duarte <guiduart3223@gmail.com>
2025-06-15 15:14:49 -0300

    DTO USUARIO

--- src/main/java/org/example/aces_api/dto/UsuarioResponseDTO.java
+++ src/main/java/org/example/aces_api/dto/UsuarioResponseDTO.java
@@ -1,4 +1,19 @@
 package org.example.aces_api.dto;
 
-public record UsuarioResponseDTO() {
+import com.fasterxml.jackson.annotation.JsonInclude;
+import org.springframework.hateoas.Links;
+
+import java.time.LocalDateTime;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public record UsuarioResponseDTO(
+        Integer id,
+        String nome,
+        String email,
+        String cargo,
+        LocalDateTime dataCriacao,
+        Links _links
+
+
+) {
 }
