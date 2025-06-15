8de349dfa999e6b887b81a6b25c83b0d5ad15a22
Guilherme Duarte <guiduart3223@gmail.com>
2025-06-15 16:43:21 -0300

    Criar model Agente Area

--- src/main/java/org/example/aces_api/model/entity/AgenteArea.java
+++ src/main/java/org/example/aces_api/model/entity/AgenteArea.java
@@ -1,4 +1,54 @@
 package org.example.aces_api.model.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "AgenteArea")
 public class AgenteArea {
+
+    @EmbeddedId
+    private AgenteAreaId id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @MapsId("agenteId")
+    @JoinColumn(name = "agente_id")
+    private Agente agente;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @MapsId("areaId")
+    @JoinColumn(name = "area_id")
+    private Area area;
+
+    public AgenteArea() {}
+
+    public AgenteArea(Agente agente, Area area) {
+        this.agente = agente;
+        this.area = area;
+        this.id = new AgenteAreaId(agente.getId(), area.getId());
+    }
+
+    // Getters e Setters
+    public AgenteAreaId getId() {
+        return id;
+    }
+
+    public void setId(AgenteAreaId id) {
+        this.id = id;
+    }
+
+    public Agente getAgente() {
+        return agente;
+    }
+
+    public void setAgente(Agente agente) {
+        this.agente = agente;
+    }
+
+    public Area getArea() {
+        return area;
+    }
+
+    public void setArea(Area area) {
+        this.area = area;
+    }
 }
