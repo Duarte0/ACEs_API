1f8770bf2b8c60bdc61185d48067c00c47387172
Guilherme Duarte <guiduart3223@gmail.com>
2025-06-15 15:13:32 -0300

    DTO para criacao de usuario

--- src/main/java/org/example/aces_api/dto/UsuarioRequestDTO.java
+++ src/main/java/org/example/aces_api/dto/UsuarioRequestDTO.java
@@ -1,4 +1,11 @@
 package org.example.aces_api.dto;
 
-public record UsuarioRequestDTO() {
-}
+import jakarta.validation.constraints.NotBlank;
+
+public record UsuarioRequestDTO(
+        @NotBlank String nome,
+        @NotBlank String cpf,
+        @NotBlank String email,
+        String telefone,
+        String cargo
+) {}
